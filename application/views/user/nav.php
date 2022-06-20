9d13cc279b990b4ba98dcbbed538e9600c4f64e4
zulfanindar <zulfanindar@student.telkomuniversity.ac.id>
2022-06-20 07:44:28 +0700

    user update

--- application/views/user/nav.php
+++ application/views/user/nav.php
@@ -1,6 +1,21 @@
 		<h5 class="light">Akun Saya</h5>
 		<ul class="collection">
+			<li class="collection-item"><a href="<?=site_url('user/schedule')?>"><i class="material-icons inline-text">schedule</i> Data Kedatangan </a></li>
+			<li class="collection-item"><a href="<?=site_url('user/schedulereverse')?>"><i class="material-icons inline-text">schedule</i> Data Keberangkatan </a></li>
 			<li class="collection-item"><a href="<?=site_url('user/profile')?>"><i class="material-icons inline-text">settings</i> Ubah Profil </a></li>
 			<li class="collection-item"><a href="<?=site_url('user/order')?>"><i class="material-icons inline-text">shopping_cart</i> Pesanan </a></li>
 			<li class="collection-item"><a href="<?=site_url('auth/logout')?>"><i class="material-icons inline-text">power_settings_new</i> Keluar </a></li>
 		</ul>
+
+
+<!-- 		<div style="margin-left:75px;"><h5 class="light" style=" margin-top:100px;">My Qr Code</h5>
+		<ul class="collection" style="width: 250px;">
+			<li class="collection-item"><a href="<?=site_url('user/schedule')?>">  <center>   <img id='barcode' 
+            src="https://api.qrserver.com/v1/create-qr-code/?data=http://localhost/TRACSESS12/user/schedule&amp;size=100x100" 
+            alt="" 
+            title="Kartu Anggotaku" 
+            width="200" 
+            height="200" 
+            /> </center> </a></li>
+			
+		</ul></div> -->
